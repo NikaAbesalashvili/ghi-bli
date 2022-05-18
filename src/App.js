a9527f54b2a140c478d52efc30d6a63c0724d4c2
NikaAbesalashvili <nikolozi.abesalashvili.1@btu.edu.ge>
2022-05-18 23:23:11 +0400

    added API endpoints in api directory

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Films from './components/Films/Films';
+
+function App() {
+    return (
+        <div className="App">
+            <Films />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'https://ghibliapi.herokuapp.com' });
+
+// FILMS
+export const getFilms = () => API.get('/films')
+export const getFilm = (filmId) => API.get(`/films/${filmId}`)
+
+// PEOPLE
+export const getPeople = () => API.get('/people');
+export const getPerson = (personId) => API.get(`/people/${personId}`);
+
+// LOCATIONS
+export const getLocations = () => API.get('/locations');
+export const getLocation = (locationId) => API.get(`/locations/${locationId}`);
+
+// SPECIES
+export const getSpecies = () => API.get('/species');
+export const getSpecie = (specieId) => API.get(`/species/${specieId}`);
+
+// VEHICLES
+export const getVehicles = () => API.get('/vehicles');
+export const getVehucle = (vehicleId) => API.get(`/vehucles/${vehicleId}`);
